6ea1ce6e67954181d8bf27e3b37a050f52da95da
DUY Ngo Van (VTI.SnP) <duy.ngovan@vti.com.vn>
2024-01-07 17:34:02 +0700

    update settings.json and procedure-options.request.ts

--- apps/api-public/src/api/api-procedure/request/procedure-options.request.ts
+++ apps/api-public/src/api/api-procedure/request/procedure-options.request.ts
@@ -1,6 +1,7 @@
 import { ApiPropertyOptional } from '@nestjs/swagger'
-import { Expose, Transform } from 'class-transformer'
-import { IsIn, IsNotEmpty, IsString } from 'class-validator'
+import { Expose, Transform, Type } from 'class-transformer'
+import { IsIn, IsNotEmpty, IsString, ValidateNested } from 'class-validator'
+import { ConditionTimestamp } from '../../../../../_libs/common/dto'
 import { SortQuery } from '../../../../../_libs/common/dto/query'
 
 export class ProcedureFilterQuery {
@@ -24,6 +25,11 @@ export class ProcedureFilterQuery {
     })
     @IsIn([0, 1])
     isActive: 0 | 1
+
+    @Expose()
+    @Type(() => ConditionTimestamp)
+    @ValidateNested({ each: true })
+    updatedAt: ConditionTimestamp
 }
 
 export class ProcedureSortQuery extends SortQuery {
